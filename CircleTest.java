6d75074abce8170a0e4785c4c4d28d21c6c6e30f
Sophia Velasco <131883280+soliauve@users.noreply.github.com>
2024-04-26 15:08:42 +0900

    Add files via upload

--- CircleTest.java
+++ CircleTest.java
@@ -5,13 +5,14 @@ class Shape { //부모 클래스 먼저 작성
 class Circle extends Shape{ //섭클래스 작성
     int radius;
 
-    public Circle(int radius){ //create an instance of circle.
+    public Circle(int radius){ //constructor: defined to create an instance of the class Circle.
+        //initialize radius, x, and y.
         this.radius = radius;
         x = 0;
         y = 0;
     }
 
-
+    //define a new method called getArea
     double getArea()
     {
         return 3.14 * radius * radius;
@@ -19,6 +20,7 @@ class Circle extends Shape{ //섭클래스 작성
 }
 
 public class CircleTest {
+    //contains main method.
     public static void main(String args[])
     {
         Circle obj = new Circle(10);
